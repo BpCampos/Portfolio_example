b99dd3a846632ee2501d6ab4e5f64fce3bc16f07
Bruno Campos <campos.bruno98@gmail.com>
2023-05-04 16:11:12 -0300

    adjusting about me section and media query

--- src/components/SobreMim/index.jsx
+++ src/components/SobreMim/index.jsx
@@ -4,8 +4,15 @@ import styles from "./SobreMim.module.css";
 export default function SobreMim() {
   return (
     <div className={styles.container}>
-      <img id="sobreMim" src="https://github.com/BpCampos.png" alt="foto de perfil" />
-      <p>
+      <div className={styles.identidade}>
+        <img id="sobreMim" src="https://github.com/BpCampos.png" alt="foto de perfil" />
+        <div>
+          <p>Olá, eu sou o Bruno Campos</p>
+          <p>Desenvolvedor Full Stack</p>
+          <p>Formado em Economia pela PUC-SP</p>
+        </div>
+      </div>
+      <p className={styles.aboutMe}>
         Recém graduado de um bootcamp de desenvolvimento full stack, estou ansioso para impulsionar minha carreira como
         desenvolvedor júnior full stack. Com uma paixão por tecnologia e uma base sólida em desenvolvimento web, estou
         estou comprometido em criar aplicativos e soluções web intuitivos e eficientes que atendam às necessidades dos
