f0fdf2a249a8145ec9cc6e65804aba3bca3f3ac2
Bruno Campos <campos.bruno98@gmail.com>
2023-05-10 14:22:13 -0300

    adding media query to language switcher

--- src/components/BotaoIdioma/index.jsx
+++ src/components/BotaoIdioma/index.jsx
@@ -3,14 +3,18 @@ import styles from "./BotaoIdioma.module.css";
 export default function BotaoIdioma({ checked, changeRadio }) {
   return (
     <section className={styles.container}>
-      <input onChange={changeRadio} type="radio" checked={checked.Brasil} value="Brasil" id="Brasil-flag" />
-      <label htmlFor="Brasil-flag">
-        <img src="../../images/flags/brasil.png" alt="bandeira do brasil" />
-      </label>
-      <input onChange={changeRadio} type="radio" checked={checked.USA} value="USA" id="USA-flag" />
-      <label htmlFor="USA-flag">
-        <img src="../../images/flags/estados-unidos.png" alt="United States flag" />
-      </label>
+      <div>
+        <input onChange={changeRadio} type="radio" checked={checked.Brasil} value="Brasil" id="Brasil-flag" />
+        <label htmlFor="Brasil-flag">
+          <img src="../../images/flags/brasil.png" alt="bandeira do brasil" />
+        </label>
+      </div>
+      <div>
+        <input onChange={changeRadio} type="radio" checked={checked.USA} value="USA" id="USA-flag" />
+        <label htmlFor="USA-flag">
+          <img src="../../images/flags/estados-unidos.png" alt="United States flag" />
+        </label>
+      </div>
     </section>
   );
 }
