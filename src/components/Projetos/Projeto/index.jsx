cf0d33e83bf5c3c044188e6597e22b4d8d81f1cd
Bruno Campos <campos.bruno98@gmail.com>
2023-05-21 15:14:41 -0300

    adding conditional rendering to projects

--- src/components/Projetos/Projeto/index.jsx
+++ src/components/Projetos/Projeto/index.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import styles from "./Projeto.module.css";
-import { Typewriter } from "react-simple-typewriter";
+import React from 'react'
+import styles from './Projeto.module.css'
+import { Typewriter } from 'react-simple-typewriter'
 
 export default function Projeto({ info, checked }) {
-  const text = <Typewriter words={info.descricao} typeSpeed={25} />;
+  const text = <Typewriter words={info.descricao} typeSpeed={25} />
 
   return (
     <>
@@ -13,26 +13,29 @@ export default function Projeto({ info, checked }) {
       </div>
       <div className={styles.about_project}>
         <p>{text}</p>
-        <h2 className={checked.Brasil ? "" : styles.hide}>tecnologias utilizadas</h2>
-        <h2 className={checked.USA ? "" : styles.hide}>Technologies used</h2>
+        <h2 className={checked.Brasil ? '' : styles.hide}>tecnologias utilizadas</h2>
+        <h2 className={checked.USA ? '' : styles.hide}>Technologies used</h2>
         <div>
           <ul className={styles.technologies}>
             {info.tecnologias.map((tecnologia) => {
-              return <li key={tecnologia}>{tecnologia}</li>;
+              return <li key={tecnologia}>{tecnologia}</li>
             })}
           </ul>
           <div className={styles.links}>
             <a href={info.github} target="_blank">
               <img src="../../images/github.png" alt="" />
             </a>
-            <a href={info.website} target="_blank">
-              <img src="../../images/link-externo.png" alt="" />
-              <span className={checked.Brasil ? "" : styles.hide}>Acesse</span>
-              <span className={checked.USA ? "" : styles.hide}>Website</span>
-            </a>
+
+            {info.website !== '' && (
+              <a href={info.website} target="_blank">
+                <img src="../../images/link-externo.png" alt="" />
+                <span className={checked.Brasil ? '' : styles.hide}>Acesse</span>
+                <span className={checked.USA ? '' : styles.hide}>Website</span>
+              </a>
+            )}
           </div>
         </div>
       </div>
     </>
-  );
+  )
 }
